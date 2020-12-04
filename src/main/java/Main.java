ee4581d6e377114179f655685d744bac26ff52cd
koushik1703 <fapra_02@hcics.simtech.uni-stuttgart.de>
2020-12-04 14:45:03 +0100

    Command Line Tool for LambdaNet.

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import lambdanet.JavaAPI$;
+import lambdanet.TypeInferenceService;
+import lambdanet.TypeInferenceService$;
+
+public class Main {
+    public static void main(String[] args) {
+        var api = JavaAPI$.MODULE$;
+        var typeInfer = TypeInferenceService$.MODULE$;
+
+        // Model Configuring
+        var modelPath = api.absPath(args[0]);
+        var modelPathForUserDefined = api.joinPath(modelPath, "models/newParsing-GAT1-fc2-newSim-decay-6");
+        var paramPath = api.joinPath(modelPathForUserDefined, "params.serialized");
+        var modelCachePath = api.joinPath(modelPathForUserDefined, "model.serialized");
+        var modelConfig = api.defaultModelConfig();
+        var parsedReposPath = api.joinPath(modelPath, "data/parsedRepos");
+
+        // Load Model
+        var model = typeInfer.loadModel(paramPath, modelCachePath, modelConfig, 8, parsedReposPath);
+
+        // Prediction Service
+        var predService = api.predictionService(model, 8, 5);
+
+        // Project Directory
+        var projectDir = args[2];
+
+        // Parsing from file Directory
+        var parsingFromFileDir = args[1];
+
+        try {
+            assert !projectDir.strip().isEmpty() : "Specified path should not be empty."; // Check Directory is not empty
+
+            var projectPath = api.absPath(projectDir);
+            var parsingFromFilePath = api.absPath(parsingFromFileDir);
+            String[] skipSet = {"node_modules"};
+
+            var results = predService.predictOnProject(projectPath, false, skipSet, parsingFromFilePath); // Prediction on Project
+            var predictionResults = new TypeInferenceService.PredictionResults(results); // Prediction Result Wrapping
+            predictionResults.prettyPrint(); // Printing Result
+        } catch (Throwable e) {
+            System.out.println("Got exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
